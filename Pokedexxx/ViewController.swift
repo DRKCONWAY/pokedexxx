c8d64e6060487694bd674bd24a81c2fb95d6a3b3
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-01 19:34:52 -0500

    Initial Commit

---
+++ Pokedexxx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedexxx
+//
+//  Created by D on 9/1/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
