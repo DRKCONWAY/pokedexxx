389769687f2a13424a046026ac6e4db671196b80
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-01 21:17:13 -0500

    configured collection view cell

--- Pokedexxx/ViewController.swift
+++ Pokedexxx/ViewController.swift
@@ -10,27 +10,45 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewFlowLayout, UICollectionViewDelegate {
 
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
     }
 
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         <#code#>
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 30
     }
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 105, height: 105)
+    }
     
     
     
