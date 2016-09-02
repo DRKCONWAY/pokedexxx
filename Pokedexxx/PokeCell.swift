857112e2ddf1792a06f29d80708f852104e98e4a
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-01 20:46:36 -0500

    created PokeCell

---
+++ Pokedexxx/PokeCell.swift
@@ -0,0 +1,51 @@
+//
+//  PokeCell.swift
+//  Pokedexxx
+//
+//  Created by D on 9/1/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexID)")
+    }
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
