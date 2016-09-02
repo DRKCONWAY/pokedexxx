ead0fa6d35199f55b6e3c131dad9e41573406dfc
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-01 20:17:35 -0500

    a

---
+++ Pokedexxx/Pokemon.swift
@@ -0,0 +1,54 @@
+//
+//  Pokemon.swift
+//  Pokedexxx
+//
+//  Created by D on 9/1/16.
+//  Copyright © 2016 D Conway. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexID: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexID: Int {
+        return _pokedexID
+    }
+    
+    init(name: String, pokedexID: Int) {
+        
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
