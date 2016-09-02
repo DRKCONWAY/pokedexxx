086c14c531d45e97d710d8c7f9b60b297e1eb3de
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-09-01 20:52:36 -0500

    added delegates

--- Pokedexxx/ViewController.swift
+++ Pokedexxx/ViewController.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewFlowLayout, UICollectionViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    
+    
+    
+  
+    
+    
     
     
     
